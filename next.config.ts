bbf1e202aca4e8937eb1520c10856a0006278a75
Saúl Sáyago <contacto@sayago.dev>
2025-11-06 06:25:54 -0600

    build: add unoptimized images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  images: {
+    // En export estático no existe el servidor de optimización de imágenes
+    // de Next, así que debemos desactivarlo para que sirva las imágenes tal cual
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
